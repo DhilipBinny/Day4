a523b25fed447c1d7a915e542c33dc51f97121c9
Hans <yunghans@hotmail.com>
2019-02-18 11:28:05 +0800

    Halfway through

---
+++ Day4/Program2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    class Program2
+    {
+        static void Main()
+        {
+            //int[] A = new int[5];
+            //A[0] = 100;
+            //A[1] = 200;
+            //A[2] = 300;
+
+            //for (int i=0; i < A.Length; i++)
+            //{
+            //    Console.WriteLine($"A[{i}] = {A[i]}");
+            //    Console.WriteLine(i + "-" + A[i]);
+            //}
+
+            int[] B = new int[] { 100, 200, 300, 0, 0 };
+            //int[] B = new int[]; //error
+            //int[] B = new int[3] { 100, 200, 300, 0, 0 }; //error 
+
+            Console.Write("B = ");
+            PrintArray(B);
+
+            int[] C;
+            C = B;
+            Console.Write("C = ");
+            PrintArray(C);
+
+            C[3] = 1000;
+            Console.Write("B = "); PrintArray(B);
+            Console.Write("C = "); PrintArray(C);
+
+            int[] D = new int[5];
+            D[2] = B[2];
+            Console.Write("B = "); PrintArray(B);
+            Console.Write("D = "); PrintArray(D);
+
+            //make a copy
+            int[] E = new int[B.Length]; //create an array with same size as B
+            Array.Copy(B, E, B.Length);
+            Console.Write("B = "); PrintArray(B);
+            Console.Write("E = "); PrintArray(E);
+            E[3] = 50000;
+            Console.Write("B = "); PrintArray(B);
+            Console.Write("E = "); PrintArray(E);
+
+
+        }
+
+        public static void PrintArray(int[] A)
+        {
+            Console.Write("{");
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.Write($"{A[i]}");
+                if (i<A.Length - 1)
+                {
+                    Console.Write(",");
+                }
+            }
+            Console.WriteLine("}");
+        }
+    }
+}
---
+++ Day4/Program3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    class Program3
+    {
+        static void Main()
+        {
+            int[] array = new int[] { 3, 2, 1, 4, 0, 5 };
+            Program2.PrintArray(array);
+
+            //green arrow start from the beginning to the 2nd last element
+            for (int green = 0; green < array.Length - 1; green++)
+            {
+                //red arrow start from one element after green arrow
+                //and ends at the last element
+                //last element : (red < array.Length) OR (red <= array.Length - 1)
+                for (int red = green +1; red < array.Length; red++)
+                {
+                    //compare
+                    //if values pointed by red arrow is smaller than values
+                    //pointed by green arrow, we swap them
+                    if (array[red] > array[green])
+                    {
+                        //swap
+                        int temp = array[red];
+                        array[red] = array[green];
+                        array[green] = temp;
+                    }
+                }
+            }
+            Program2.PrintArray(array);
+
+        }
+    }
+}
